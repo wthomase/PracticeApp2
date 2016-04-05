3d7f04a1c19cd43997d28350afd3a602609f7c95
t w <wthomase@myuw.edu>
2016-04-05 09:35:54 -0700

    Initial commit.

---
+++ app/src/test/java/tcss450/uw/edu/practiceapp2/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package tcss450.uw.edu.practiceapp2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
